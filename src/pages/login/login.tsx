72c0a635afbc3e1b3b46de7fba905df6999cf208
Aniket1308-dev <aniketdevadas@gmail.com>
2026-01-16 21:42:21 +0530

    add mutation for logout

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -27,6 +27,14 @@ const LoginPage = () => {
         enabled: false,
     });
 
+    const { mutate: logoutMutate } = useMutation({
+        mutationKey: ['logout'],
+        mutationFn: logout,
+        onSuccess: async () => {
+            logoutFromStore();
+            return;
+        },
+    });
 
     const { mutate, isPending, isError, error } = useMutation({
         mutationKey: ['login'],
@@ -37,8 +45,7 @@ const LoginPage = () => {
             // window.location.href = "http://clientui/url"
             // "admin", "manager", "customer"
             if (!isAllowed(selfDataPromise.data)) {
-                await logout();
-                logoutFromStore();
+                logoutMutate();
                 return;
             }  
 
